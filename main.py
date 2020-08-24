2bc674b840e051258d2311bc99ca8742d413405e
TheCrypticMusic <44878496+TheCrypticMusic@users.noreply.github.com>
2020-08-24 14:50:36 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+class JobScraper:
+    
+    results = []
+    
+    
+    def __init__(self, job, location):
+        self.job = job
+        self.location = location
+        self.url = f'https://www.indeed.co.uk/jobs?q={self.job}&l={self.location}'
+        
+    
+    def connect(self):
+        self.res = self.res = requests.get(self.url)
+        if self.res.status_code == 200:
+            return self.res
+        return None
+    
+    
+    def search(self):
+        self.__soup_contents()
+    
+    def __soup_contents(self):
+        self.soup = BeautifulSoup(self.connect().text, 'lxml')
+        self.jobs = self.soup.find_all('div', {'class': 'summary'})
+        for job in self.jobs:
+            self.results.append(job)
+            
