5879610fa22ec6bbd301a00e04696fb8126b338e
Daniel Gibson <gibsonlp@live.co.uk>
2020-09-08 21:42:41 +0100

    fixes

--- jobscraper.py
+++ jobscraper.py
@@ -10,8 +10,9 @@ class JobScraper:
     def __init__(self, job, location):
         self.job = job
         self.location = location
-        self.url = [#f'https://www.indeed.co.uk/jobs?q={self.job}&l={self.location}',
+        self.url = [f'https://www.indeed.co.uk/jobs?q={self.job}&l={self.location}',
                     f'https://www.monster.co.uk/jobs/search/?q={self.job}&where={self.location}&client=power&cy=uk&rad=25']
+        
 
     def connect(self):
         for website in self.url:
@@ -23,7 +24,7 @@ class JobScraper:
 
     def search(self):
         print(f'Searching for {self.job} jobs in {self.location}')
-        # self.__indeed_soup_contents()
+        self.__indeed_soup_contents()
         self.__monster_soup_contents()
 
     def __indeed_soup_contents(self):
@@ -32,14 +33,14 @@ class JobScraper:
         for i in jobs:
             title = i.find('h2', {'class': 'title'}).text.splitlines()
             company = i.find('div', {'class': 'sjcl'}).text.splitlines()
-            formatted_company = ("".join(company).replace(f'{self.location}', ' '))
+            formatted_company = ("".join(company).replace(f'{self.location}', f'\n{self.location}'))
             summary = i.find('div', {'class': 'summary'}).text.splitlines()
             job_links = i.find('h2', {'class': 'title'}).a['href']
 
             print("\n".join(title))
             print(re.sub(r'[0-9]+', '', formatted_company))
             print("".join(summary))
-            print(f'Click to see more: https://indeed.co.uk{job_links}')
+            print(f'Click to see more: https://indeed.co.uk{job_links:.40}')
         # while True:
         #     print("\nWould you like to view the next or previous page? ('Next'/'Previous')")
         #     user_input = input('>>>> ')
@@ -57,11 +58,11 @@ class JobScraper:
             title = i.find('h2', {'class': 'title'}).text.splitlines()
             company = i.find('div', {'class': 'company'}).text.splitlines()
             location = i.find('div', {'class': 'location'}).text.splitlines()
-            url = i.find('h2', {'class': 'title'}).href
+            url = i.find('h2', {'class': 'title'})
             print("".join(title))
             print("".join(company))
             print("".join(location))
-            print(url)
+            print(url.a['href'])
             print('\n')
 
     def __next_page(self):
